78222702e720de18a52a54a7416e13bf71e79e98
John Bailey <jbailey@legendseeds.net>
2025-03-27 19:51:11 -0500

    Fix: add cn utility function to utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,3 @@
-// lib/utils.ts
-export function cn(...classes: (string | undefined | false)[]) {
-  return classes.filter(Boolean).join(' ');
+export function cn(...classes: (string | false | null | undefined)[]): string {
+  return classes.filter(Boolean).join(" ");
 }
