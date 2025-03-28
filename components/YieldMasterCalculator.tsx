826a26e85358977f72274ce4796ba37287bea46a
John Bailey <jbailey@legendseeds.net>
2025-03-27 21:21:21 -0500

    Fix: correct html2canvas import syntax

--- components/YieldMasterCalculator.tsx
+++ components/YieldMasterCalculator.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useState, useRef } from 'react';
-import * as html2canvas from 'html2canvas';
+import html2canvas from 'html2canvas';
 import jsPDF from 'jspdf';
 
 import { Card, CardHeader, CardTitle, CardContent } from './ui/card';
